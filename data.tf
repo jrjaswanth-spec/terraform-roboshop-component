a0be88f1f1db47555aded67826c4f1121959e171
Jaswanth <jrjaswanth@gmail.com>
2026-05-25 18:37:58 +0530

    ffcat

--- data.tf
+++ data.tf
@@ -38,7 +38,7 @@ data "aws_ssm_parameter" "frontend_alb_listener_arn" {
   
 }
 
-data "aws_ssm_parameter" "private_subnet_id" {
+data "aws_ssm_parameter" "private_subnet_ids" {
   name =  "/${var.project_name}/${var.environment}/private_subnet_ids"
   
 }
