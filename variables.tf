96061a66449f662219ea2e253c6ebb6be10ae8c7
Jaswanth <jrjaswanth@gmail.com>
2026-05-25 17:32:46 +0530

    dcat

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "jrdaws.life"
+  
+}
+
+variable "component" {
+  
+}
+
+variable "rule_priority" {
+  
+}
